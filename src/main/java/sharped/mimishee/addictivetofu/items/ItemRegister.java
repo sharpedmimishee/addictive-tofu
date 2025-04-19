019bfa046033c745f1cb06c3489681e2d0b2b62a
baguchan <shutux500@gmail.com>
2025-04-19 18:59:47 +0900

    tf sword

--- src/main/java/sharped/mimishee/addictivetofu/items/ItemRegister.java
+++ src/main/java/sharped/mimishee/addictivetofu/items/ItemRegister.java
@@ -1,12 +1,14 @@
 package sharped.mimishee.addictivetofu.items;
 
 
+import baguchan.tofucraft.registry.TofuItemTier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import sharped.mimishee.addictivetofu.AddictiveTofu;
+import sharped.mimishee.addictivetofu.items.tfenergy.TFSwordItems;
 
 public class ItemRegister {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(AddictiveTofu.MODID);
@@ -15,6 +17,8 @@ public class ItemRegister {
             () -> new Item(new Item.Properties().rarity(Rarity.EPIC).stacksTo(64)));
     public static final DeferredItem<Item> ADV_TOFU = ITEMS.register("adv_tofu",
             () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> TF_SWORD = ITEMS.register("tf_sword",
+            () -> new TFSwordItems(TofuItemTier.TOFUDIAMOND, new Item.Properties().attributes(TFSwordItems.createAttributes(TofuItemTier.TOFUDIAMOND, 4, -2.3F)).stacksTo(1)));
     public static final DeferredItem<Item> ACTIVE_NULL_TOFU = ITEMS.register("active_null_tofu",
             () -> new Item(new Item.Properties().rarity(Rarity.EPIC).stacksTo(64)));
 //    public static final DeferredItem<Item> ZUNDA_MIRROR = ITEMS.register("zunda_mirror",
