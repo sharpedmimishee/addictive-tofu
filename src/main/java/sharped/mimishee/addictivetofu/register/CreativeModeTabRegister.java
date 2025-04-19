ab4068424f286201c43385b3ff9cee02385c370f
baguchan <shutux500@gmail.com>
2025-04-19 17:46:29 +0900

    adv tofu in creative tab

--- src/main/java/sharped/mimishee/addictivetofu/register/CreativeModeTabRegister.java
+++ src/main/java/sharped/mimishee/addictivetofu/register/CreativeModeTabRegister.java
@@ -15,8 +15,7 @@ import java.util.stream.Stream;
 public class CreativeModeTabRegister {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AddictiveTofu.MODID);
     public static final Supplier<CreativeModeTab> ADDTICTIVE_TOFU = CREATIVE_MODE_TABS.register(AddictiveTofu.MODID, () -> CreativeModeTab.builder().withTabsBefore(CreativeModeTabs.SPAWN_EGGS).title(Component.translatable("itemGroup." + AddictiveTofu.MODID)).icon(() -> ItemRegister.ZUNDA_CROSSBOW.get().asItem().getDefaultInstance()).displayItems((parameters, output) -> {
-        output.acceptAll(Stream.of(ItemRegister.ZUNDA_CROSSBOW, ItemRegister.NULL_TOFU, ItemRegister.ACTIVE_NULL_TOFU).map((sup) -> sup.get().asItem().getDefaultInstance()).toList());
-        output.acceptAll(Stream.of(ItemRegister.ZUNDA_CROSSBOW, ItemRegister.NULL_TOFU).map((sup) -> sup.get().asItem().getDefaultInstance()).toList());
+        output.acceptAll(Stream.of(ItemRegister.ZUNDA_CROSSBOW, ItemRegister.ADV_TOFU, ItemRegister.NULL_TOFU, ItemRegister.ACTIVE_NULL_TOFU).map((sup) -> sup.get().asItem().getDefaultInstance()).toList());
         output.acceptAll(Stream.of(BlockRegister.ADV_TOFU_BARREL, BlockRegister.ADV_TOFU_BLOCK).map((sup) -> sup.get().asItem().getDefaultInstance()).toList());
     }).build());
 }
